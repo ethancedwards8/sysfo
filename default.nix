1d585b67a857c719b7ecc747371ca087975c413f
Magic_RB <magic_rb@redalder.org>
2021-03-03 19:35:12 +0100

    Fix flake-compat
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- default.nix
+++ default.nix
@@ -1,2 +1,7 @@
-final: prev:
-(import ./flake-compat.nix).overlay final prev
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).defaultNix
